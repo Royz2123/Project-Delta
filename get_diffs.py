9f1ba711a969b4a6ff62d122314ceb8f9c8d35b5
Royz2123 <1Qazwsxdc>
2019-04-21 20:24:27 +0300

    Added basic suspicious item filtering

--- get_diffs.py
+++ get_diffs.py
@@ -4,12 +4,44 @@ import os
 import image_processing
 
 
-
 DIFF_METHOD = 0
+SIMILARITY_THRESH = 0.5
+
+
+# Class that represents the suspicious item
+class SuspiciousItem(object):
+    # If item has been in frame for MIN_STEPS, keep until end of run
+    MIN_STEPS = 100
+
+    # If item has been spotted in MIN_RATIO frames, keep currently
+    MIN_RATIO = 0.7
+
+    def __init__(self, cnt):
+        self._cnt = cnt
+        self._stayed = 0
+        self._moved = 0
+
+    def keep_item(self):
+        ratio = self._stayed / (self._stayed + self._moved)
+        if self._stayed > SuspiciousItem.MIN_STEPS or ratio > SuspiciousItem.MIN_RATIO:
+            return True
+        return False
+
+    def get_contour(self):
+        return self._cnt
+
+    def update_item_status(self, stay):
+        if stay:
+            self._stayed += 1
+        else:
+            self._moved += 1
+
 
 def main():
     session_path = "sessions/" + max(os.listdir("sessions/")) + "/"
     session_images = [session_path + path for path in sorted(os.listdir(session_path))]
+
+    suspicious_items = []
 
     for i in range(len(session_images) - 1):
         im1 = cv2.imread(session_images[i])
@@ -17,11 +49,30 @@ def main():
 
         # Call diff method
         diff = image_processing.DIFF_METHODS[DIFF_METHOD](im1, im2)
+        cnts, diff = image_processing.get_contours(diff)
+
+        # Add the new contours to the list of suspicious items
+        suspicious_items += [SuspiciousItem(cnt) for cnt in cnts]
+
+        # check which contours stayed from last time
+        new_items = []
+        for item in suspicious_items:
+            similarity = image_processing.contour_stayed(im1, im2, item.get_contour())
+            item.update_item_status(similarity > SIMILARITY_THRESH)
+            if item.keep_item():
+                new_items.append(item)
+        suspicious_items = new_items
 
         cv2.imwrite("demos/demo" + str(i) + ".jpg", diff)
         cv2.imshow("Difference ", diff)
         cv2.waitKey(0)
 
+    # output the final differences
+    output = cv2.imread(session_images[0])
+    output = image_processing.draw_rect_contours(output, [item.get_contour() for item in suspicious_items])
+    cv2.imwrite("demos/final.jpg", output)
+    cv2.imshow("Output", output)
+    cv2.waitKey(0)
 
 
 
--- image_processing.py
+++ image_processing.py
@@ -1,7 +1,9 @@
 from skimage.measure import compare_ssim
 import imutils
 import cv2
+import numpy as np
 
+MIN_CNT_SIZE = 50
 
 # Diff methods get 2 images as input, and output the diff matrix
 
@@ -14,16 +16,69 @@ def diff_method1(im1, im2):
     # images, ensuring that the difference image is returned
     (score, diff) = compare_ssim(grayA, grayB, full=True)
     diff = (diff * 255).astype("uint8")
-    # print("SSIM: {}".format(score))
-
-    # threshold the difference image, followed by finding contours to
-    # obtain the regions of the two input images that differ
-    thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-    return thresh
+    return diff
 
 
 def diff_method2(im1, im2):
     return cv2.absdiff(im1, im2)
+
+
+def draw_rect_contours(img, cnts):
+    for c in cnts:
+        # compute the bounding box of the contour and then draw the
+        # bounding box on both input images to represent where the two
+        # images differ
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    return img
+
+
+def get_contours(diff):
+    # threshold the difference image, followed by finding contours to
+    # obtain the regions of the two input images that differ
+    thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    cnts = imutils.grab_contours(cnts)
+
+    # loop over the contours
+    final_cnts = [cnt for cnt in cnts if len(cnt) >= MIN_CNT_SIZE]
+    draw_rect_contours(diff, final_cnts)
+
+    return final_cnts, diff
+
+
+# Compares two images regarding a certain contour
+def mse(im1, im2, normalization):
+    # the 'Mean Squared Error' between the two images is the
+    # sum of the squared difference between the two images;
+    # NOTE: the two images must have the same dimension
+    im1 = im1.astype("float") / float(255)
+    im2 = im2.astype("float") / float(255)
+    err = np.sum((im1 - im2) ** 2) / float(normalization)
+
+    # return the MSE, the lower the error, the more "similar"
+    # the two images are
+    return err
+
+
+def contour_stayed(im1, im2, cnt):
+    height, width, depth = im1.shape
+
+    masks = []
+    for img in [im1, im2]:
+        mask = np.zeros((height, width), np.uint8)
+        cv2.drawContours(mask, [cnt], 0, 1, 3)
+        masked = cv2.bitwise_and(img, img, mask=mask)
+
+        #cv2.imshow("masked", img)
+        #cv2.waitKey(0)
+        #cv2.imshow("masked", masked)
+        #cv2.waitKey(0)
+
+        masks.append(masked)
+
+    similarity = mse(masks[0], masks[1], len(cnt))
+    return similarity
 
 
 DIFF_METHODS = [
